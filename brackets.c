5ac97472fbd447262cca51d47190303c78bdf2d5
chi <nguyenlinhchi09@gmail.com>
2018-09-28 05:43:19 +0200

    redo

--- brackets.c
+++ brackets.c
@@ -5,6 +5,15 @@ void ft_putchar(char c)
   write(1,&c,1);
 }
 
+int length(char *str)
+{
+  int i;
+  i = 0;
+  while (str[i])
+    ++i;
+  return i;
+}
+
 int open_bracket(char c)
 {
   if (c == '(')
@@ -27,33 +36,32 @@ int close_bracket(char c)
   return 0;
 }
 
-int correct_pair(int i, char *str)
-{
-  int j;
-  j = i + 1;
-  while (str[j])
-    {
-      if (close_bracket(str[j]) == open_bracket(str[i]))
-	return 1;
-      ++j;
-    }
-  return 0;
-}
-
 int correct(char *str)
 {
+  int l = length(str);
+  if (l == 0)
+    return 1;
+  int array[l];
   int i;
   i = 0;
+  int j = 0;
   while (str[i])
     {
       if (open_bracket(str[i]))
 	{
-	  if (!(correct_pair(i, str)))
+	  array[j] = open_bracket(str[i]);
+	  ++j;
+	}
+      if (close_bracket(str[i]))
+	{
+	  if (array[j - 1] != close_bracket(str[i]))
 	    return 0;
+	  else
+	      array[j--] = 0;
 	}
       ++i;
     }
-  return 1;	
+  return 1;
 }
 
 int main(int av, char **ac)
@@ -64,7 +72,7 @@ int main(int av, char **ac)
   i = 1;
   while (i < av)
     {
-      if (correct(ac[i]) == 1)
+      if (correct(ac[i]))
 	write(1, "OK\n", 3);
       else
 	write(1, "Error\n", 6);
